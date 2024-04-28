5665767e051db3789e6bc7a317df55f622425b2e
guoqiao <guoqiao@gmail.com>
2024-04-29 11:50:25 +1200

    add more pkgs in scoop-install.bat

--- scoop-install.bat
+++ scoop-install.bat
@@ -1,1 +1,11 @@
-scoop install touch bat btop aria2 eza lsd gping neovim
+REM Install packages with scoop
+
+REM If exists, scoop will use aria2 to download other pkgs
+scoop install aria2
+
+scoop install ^
+touch bat delta neovim ^
+eza lsd ag fd zoxide ^
+dust duf broot ^
+btop bottom procs ^
+dog gping curlie
