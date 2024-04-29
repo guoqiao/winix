6c7f2efd8ad8b3c906c632917a43bb3f6a46820d
guoqiao <guoqiao@gmail.com>
2024-04-29 19:57:17 +1200

    make chext.py more general

--- bin/chext.py
+++ bin/chext.py
@@ -1,15 +1,31 @@
 import os
 import sys
 
-# Set the directory containing the files
-directory = "."  # Change this to your directory
+args = sys.argv
+assert len(sys.argv) >= 2
 
+def fmt(ext):
+    """Ensure ext format in lower case and starts with ., ex: .bat"""
+    return '.' + ext.strip().strip('.').lower()
+
+from_ext = fmt(args[0])
+to_ext = fmt(args[1])
+
+if len(args) == 3:
+    root = args[2]
+else:
+    root = '.'
+
+
+total = 0
 # Loop through all files in the directory
-for filename in os.listdir('.'):
-    # Check if the file has a .bat extension
-    if filename.lower().endswith(".bat"):
+for filename in os.listdir(root):
+    # Check if the file has a expected extension
+    if filename.lower().endswith(from_ext):
         # Construct the new filename with a .cmd extension
-        new_filename = os.path.splitext(filename)[0] + ".cmd"
+        new_filename = os.path.splitext(filename)[0] + to_ext
         # Rename the file
         os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
         print(f"Renamed {filename} to {new_filename}")
+        total += 1
+print(f'total renamed: {total}')
